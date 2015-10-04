e38ceffc30ed8be8eaa3cba96339db897c355f7c
rokikon <Romakiro@gmail.com>
2015-10-05 00:25:12 +0300

    Update newsletter.py

--- emencia/django/newsletter/utils/newsletter.py
+++ emencia/django/newsletter/utils/newsletter.py
@@ -38,10 +38,10 @@ def track_links(content, context):
             link_title = link_markup.get('title', link_href)
             link, created = Link.objects.get_or_create(url=link_href,
                                                        defaults={'title': link_title})
-            link_markup['href'] = 'http://%s%s' % (context['domain'], reverse('newsletter_newsletter_tracking_link',
-                                                                              args=[context['newsletter'].slug,
-                                                                                    context['uidb36'], context['token'],
-                                                                                    link.pk]))
+            # link_markup['href'] = 'http://%s%s' % (context['domain'], reverse('newsletter_newsletter_tracking_link',
+            #                                                                   args=[context['newsletter'].slug,
+            #                                                                         context['uidb36'], context['token'],
+            #                                                                         link.pk]))
     if USE_PRETTIFY:
         return soup.prettify()
     else:
