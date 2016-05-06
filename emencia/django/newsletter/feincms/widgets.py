6607f7331f6a806190be435cc0b2119ce63a06c3
Ivan Sysoi <ivan.sysoi@gmail.com>
2016-05-07 02:45:17 +0300

    Add feinCMS widget to subscribe.

---
+++ emencia/django/newsletter/feincms/widgets.py
@@ -0,0 +1,24 @@
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
+
+from emencia.django.newsletter.models import MailingList
+
+
+class SubscribeWidget(models.Model):
+    title = models.CharField(_('title'), max_length=50, blank=True)
+    mailing_list = models.ForeignKey(MailingList, verbose_name=_('mailing list'))
+
+    class Meta:
+        abstract = True
+        verbose_name = _('Subscribe Widget')
+
+    @property
+    def subscribe_url(self):
+        return reverse('newsletter_mailinglist_subscribe', mailing_list_id=self.mailing_list.id)
+
+    def render(self, **kwargs):
+        return render_to_string('newsletter/feincms/subscribe_widget.html',
+                                {'content': self},
+                                context_instance=kwargs.get('context'))
