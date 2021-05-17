156a5c02be59493d22f3df25b3fe405ca83c8e7b
anaelorlinski <ao@m9x.fr>
2021-05-17 16:33:33 +0200

    Missing templates in package

--- aoml/__init__.py
+++ aoml/__init__.py
@@ -1,8 +1,8 @@
 """aoml"""
-__version__ = '0.1'
+__version__ = '0.92'
 __license__ = 'BSD License'
 
 __author__ = 'AO'
 __email__ = 'ao@m9x.fr'
 
-__url__ = ''
+__url__ = 'https://github.com/anaelorlinski/django-aoml'
